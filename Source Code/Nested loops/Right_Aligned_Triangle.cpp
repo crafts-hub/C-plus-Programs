88a009eb4a151b736ba1940fbc53fd15c9ae5f70
Jameel_499 <jamilraza500@gmail.com>
2025-05-27 01:08:09 -0700

    Updated Print Pattern Programs

--- Source Code/Nested loops/Right_Aligned_Triangle.cpp
+++ Source Code/Nested loops/Right_Aligned_Triangle.cpp
@@ -5,7 +5,7 @@ int main() {
     int n = 5; // Height of the triangle
     for (int i = 1; i <= n; i++) 
     {
-        for (int j = 1; j <= n - i; j++) 
+        for (int j = n; j > i; j--) // for other programs, I did "<=" or ">=". But in this one, I did ">" only, the reason is: if (n = 5) then I want to put 4 spaces and then one star
         {
             cout << " ";  // Printing spaces for right alignment
         }
